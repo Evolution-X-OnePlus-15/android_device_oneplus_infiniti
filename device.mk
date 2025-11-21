bdf621775ba91f7cc4e44bbc03cd13a582e9e888
Pranav Vashi <neobuddy89@gmail.com>
2025-11-22 01:39:40 +0530

    infiniti: Include LTPO settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ TARGET_SCREEN_HEIGHT := 2772
 TARGET_SCREEN_WIDTH := 1272
 
 # Display
+PRODUCT_PACKAGES += \
+    OplusLtpo
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946903293830803.xml \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946982335253651.xml
