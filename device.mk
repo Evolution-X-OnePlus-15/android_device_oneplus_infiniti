74cd744086bca327341a93840244c2de2192676d
Jonas Salo <salomannen1234@gmail.com>
2026-06-28 01:05:54 +0200

    infiniti: add camera port build compatibility flags
    Change-Id: Ic42555f278bab81f0add3f2c06bf2f6490aa6ecb

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 DEX_PREOPT_DEFAULT := speed-profile
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
+# Device is 4K-pages (kernel CONFIG_ARM64_4K_PAGES=y); declare it so check_elf_file
+# does not require the 16K-readiness alignment that older 4K OEM camera JNI libs lack.
+PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 4096
+
 # Display
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946850534658451.xml \
