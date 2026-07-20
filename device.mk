d871b20f06d12df00e9440ac51e1660f5daf440c
1vivy <1vivy@tutanota.com>
2026-07-20 15:40:47 -0600

    infiniti: add Dolby Vision DVS service contract
    Change-Id: I92567712d23abedb03586fac92def9fc39ca5ba6

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.oplus.product.series=flagship_series \
     ro.product.supported_versions=24863
 
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    device/oneplus/infiniti/vintf/device_framework_matrix.xml
+
 # Recovery
 $(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
 
